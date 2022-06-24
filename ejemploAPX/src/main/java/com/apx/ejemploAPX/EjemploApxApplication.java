5cd56da4a55aa23689b379ef5b97f4dfdcd2c381
dhuamanr01 <dhuamanr01@gmail.com>
2022-06-24 09:04:54 -0500

    primer commit reto APX

---
+++ ejemploAPX/src/main/java/com/apx/ejemploAPX/EjemploApxApplication.java
@@ -0,0 +1,13 @@
+package com.apx.ejemploAPX;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EjemploApxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EjemploApxApplication.class, args);
+	}
+
+}
---
+++ ejemploAPX/src/test/java/com/apx/ejemploAPX/EjemploApxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apx.ejemploAPX;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EjemploApxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
